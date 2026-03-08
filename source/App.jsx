a0f0046d69465abb85480b8982e85ec1276b49bf
Legend <thesfmgod@gmail.com>
2026-03-07 22:54:49 -0500

    chore/docs(application): assign roles for gameplay implementation

--- source/App.jsx
+++ source/App.jsx
@@ -1,4 +1,21 @@
-export default function App() { 
+export default function App() {
+     /**
+      * @todo: Implement health, logging, and player controls.
+      * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+      * ASSIGNMENTS
+      * -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+      * Health - @F4LDR
+      * Player Controls - @Noah-Ram52
+      * Logging - @AnimatingLegend
+     */
+     
+     /**
+      * The entire playstate.
+      * - Player Controls / Combat
+      * - Boss Fight
+      * - The Entire HUD.
+      * @returns {JSX.Element} The entire game
+      */
      return (
           <>
                <div align="center">
