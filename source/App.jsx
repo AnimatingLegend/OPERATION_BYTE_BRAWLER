26ce6e68a0abd8779c975f2a77f8c574c4dc3b0b
Legend <thesfmgod@gmail.com>
2026-03-07 19:47:38 -0500

    chore/feat: setup react

---
+++ source/App.jsx
@@ -0,0 +1,10 @@
+export default function App() { 
+     return (
+          <>
+               <div align="center">
+                    <h1>HELLO WORLD!</h1>
+                    <p>GAME IN PROGRESS. PLEASE WAIT PATIENTLY! :-]</p>
+               </div>
+          </>
+     );
+};
