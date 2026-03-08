d229ee2eea7a0c737a30b662b27273aec3625f3e
Legend <thesfmgod@gmail.com>
2026-03-08 01:58:33 -0500

    docs(cell): assign @chowchu to implement map icons

--- source/components/Cell.jsx
+++ source/components/Cell.jsx
@@ -24,7 +24,7 @@ const Cell = ({ type }) => {
      const getIcon = () => {
           /**
            * @todo - Add icons for each entity type
-           * @author @F4LDR
+           * @author @F4LDR & @ChowChu
            */
           switch (type) {
                case ENTITY_TYPES.PLAYER: return '';
