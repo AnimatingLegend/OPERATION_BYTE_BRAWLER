6a9d9cdda3492e5ae01013c379158943b38dbcd2
Legend <thesfmgod@gmail.com>
2026-03-19 23:14:42 -0400

    feat: menu placeholder complete

--- source/scenes/Menu.js
+++ source/scenes/Menu.js
@@ -1,6 +1,9 @@
 import { AudioManager } from "../utils/AudioManager";
 import { AssetLoader } from "../utils/AssetLoader";
 
+import { GameScene } from './GameScene';
+import { CreditsScene } from './CreditsScene';
+
 const ITEMS = ['Start Game', 'Donate', 'Credits'];
 
 /* -----------------------------------------------*
@@ -19,12 +22,35 @@ export class MenuScene {
       * @param {*} delta 
       * @param {*} input 
       */
-     update(delta, input) { }
+     update(delta, input) {
+          // Fade in to the menu.
+          this.alpha = Math.min(1, this.alpha + delta * 2.5);
+
+          // Menu Navigation.
+          if (input.justPressed('ArrowUp'))
+               this.selected = (this.selected - 1 + ITEMS.length) % ITEMS.length;
+          if (input.justPressed('ArrowDown'))
+               this.selected = (this.selected + 1) % ITEMS.length;
+          if (input.justPressed('Enter'))
+               this.confirm();
+     }
 
      /**
       * Confirm the selection
       */
-     confirm() { }
+     confirm() {
+          switch (this.selected) {
+               case 0:
+                    this.game.switchScene(new GameScene(this.game));
+                    break;
+               case 1:
+                    if (ITEMS[this.selected] === 'Donate') window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank');
+                    break;
+               case 2:
+                    this.game.switchScene(new CreditsScene(this.game, { returnTo: 'menu' }));
+                    break;
+          }
+     }
 
      /**
       * Render the menu.
@@ -36,18 +62,36 @@ export class MenuScene {
           context.save();
           context.globalAlpha = this.alpha;
 
-          // Background [THIS IS SUBJECT TO CHANGE]!!!
-          context.fillStyle = '#0a0a1a';
-          context.fillRect(0, 0, WIDTH, HEIGHT);
+          context.fillStyle = '#ffffff';
+          context.font = '500 48px monospace';
+          context.textAlign = 'center';
+          context.fillText('OPERATION: BYTE_BRAWLERS', WIDTH / 2, 140);
+
+          ITEMS.forEach((item, index) => {
+               const yAxis = 250 + index * 50;
+               const isSelected = index === this.selected;
+
+               if (isSelected) {
+                    context.fillStyle = 'rgba(255, 255, 255, 0.5)';
+                    context.beginPath();
+                    context.roundRect(WIDTH / 2 - 200, yAxis - 22, 240, 38, 8);
+                    context.fill();
+               }
+
+               context.fillStyle = isSelected ? '#f0c040' : '#666';
+               context.font = isSelected ? '500 22px monospace' : '400 20px monospace';
+               context.fillText(isSelected ? `> ${item} <` : item, WIDTH / 2, yAxis);
+          });
+
+          context.fillStyle = '#444';
+          context.font = '13px monospace';
+          context.fillText('Arrow keys to navigate   Enter to select', WIDTH / 2, HEIGHT - 24);
+          context.restore();
      }
 
      /**
-      * Called by the scene manager when entering this state.
+      * Called by the scene manager when entering and exiting this state.
       */
-     onEnter() { }
-
-     /**
-      * Called by the scene manager when leaving this state.
-      */
-     onExit() { /** Do nothing. */ }
+     onEnter() { /*AudioManager.playLoop('menuMusic', 0.5);*/ }
+     onExit() { /*AudioManager.playLoop('menuMusic', 0.5);*/ }
 };
