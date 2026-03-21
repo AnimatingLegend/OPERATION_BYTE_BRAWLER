7087581071ce221d37d5427c25a6d0a666b44b66
Legend <thesfmgod@gmail.com>
2026-03-21 00:41:06 -0400

    style(entity): proper code formatting | variable rename

--- source/entities/Entity.js
+++ source/entities/Entity.js
@@ -1,15 +1,21 @@
 export class Entity {
      constructor(x, y, width, height) {
-          this.x = x; this.y = y;
-          this.width = width; this.height = height;
-          this.velocityX = 0; this.velocityY = 0;
+          this.xAxis = x;
+          this.yAxis = y;
+
+          this.width = width;
+          this.height = height;
+
+          this.velocityX = 0;
+          this.velocityY = 0;
+
           this.alive = true;
      }
 
      /**
       * Shared AABB bounds - used for collision detection
       */
-     getBounds() { return { x: this.x, y: this.y, width: this.width, height: this.height }; }
+     getBounds() { return { x: this.xAxis, y: this.yAxis, width: this.width, height: this.height }; }
 
      /**
       * Every entity must implement `update()` or `render()`
